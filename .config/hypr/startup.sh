f18e815930d0685605907479aba5f4ef3a07145c
Akash <akashmondalnl@gmail.com>
2025-03-02 13:35:36 +0100

    Added hyprland

---
+++ .config/hypr/startup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Check if pipewire is running
+if ! pgrep -x "pipewire" > /dev/null; then
+  exec pipewire &
+fi
+
+# Check if pipewire-pulse is running
+if ! pgrep -x "pipewire-pulse" > /dev/null; then
+  exec pipewire-pulse &
+fi
+
+sleep 0.2
+
+# Check if wire-plumber is running
+if ! pgrep -x "wireplumber" > /dev/null; then
+  exec wireplumber &
+fi
+
+exec dbus-run-session Hyprland
+
