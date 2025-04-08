119b808379e50194788a2b4e5e1bac89542ef1bd
Akash <akashmondalnl@gmail.com>
2025-04-08 10:21:54 +0200

    Changed Env Variables

--- .config/zsh/00-env.zsh
+++ .config/zsh/00-env.zsh
@@ -1,8 +1,8 @@
 # Language environment (if needed)
 # export LANG=en_US.UTF-8
 
-# Preferred editor
-export EDITOR=/usr/bin/vim
+export XDG_CONFIG_HOME="$HOME/.config"
+export EDITOR="/usr/bin/vim"
 
 # Add extra PATHs if needed
 # export PATH="$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH"
