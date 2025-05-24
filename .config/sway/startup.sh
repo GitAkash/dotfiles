f614f5fe2a33d75e06fac52e02b2873d9d51d7b7
Akash <akashmondalnl@gmail.com>
2025-05-24 17:45:33 +0200

    Changed startup script again cause well audio needs sleep :(

--- .config/sway/startup.sh
+++ .config/sway/startup.sh
@@ -36,10 +36,9 @@
 # Start pipewire services if not running
 pgrep -x pipewire > /dev/null || pipewire &
 pgrep -x pipewire-pulse > /dev/null || pipewire-pulse &
+sleep 0.2
 pgrep -x wireplumber > /dev/null || wireplumber &
 pgrep -x udiskie > /dev/null || udiskie --smart-tray &
-
-sleep 0.2
 
 exec sway
 
