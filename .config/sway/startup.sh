04e0cf2dbb368b27f9d0d711976a81c0a1f8f969
Akash <akashmondalnl@gmail.com>
2025-05-24 17:46:14 +0200

    Startup script cleanup

--- .config/sway/startup.sh
+++ .config/sway/startup.sh
@@ -1,35 +1,5 @@
 #!/bin/sh
 
-# Load sway environment variables
-#if [ -f "$HOME/.config/sway/env" ]; then
-#  . "$HOME/.config/sway/env"
-#fi
-
-# Check if pipewire is running
-#if ! pgrep -x "pipewire" > /dev/null; then
-#  exec pipewire &
-#fi
-
-# Check if pipewire-pulse is running
-#if ! pgrep -x "pipewire-pulse" > /dev/null; then
-#  exec pipewire-pulse &
-#fi
-
-#sleep 0.2
-
-# Check if wire-plumber is running
-#if ! pgrep -x "wireplumber" > /dev/null; then
-#  exec wireplumber &
-#fi
-
-#if ! pgrep -x "udiskie" > /dev/null; then
-#  exec udiskie --smart-tray &
-#fi
-
-#exec dbus-run-session sway
-
-#!/bin/sh
-
 # Load environment vars
 [ -f "$HOME/.config/sway/env" ] && . "$HOME/.config/sway/env"
 
