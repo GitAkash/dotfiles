b5e9cfd0832ce6e67928152a76fdfeca47b0103f
Akash <akashmondalnl@gmail.com>
2025-10-26 21:38:53 +0100

    Added the gtk and qt configs

--- .config/hypr/batterywarning.sh
+++ .config/hypr/batterywarning.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
 while true; do
-    battery=$(upower -i "$(upower -e | grep BAT)" | grep -E "percentage" | awk '{print $2}' | tr -d '%')
-    if [ "$battery" -le "20" ]; then
-        notify-send -u critical "Low Battery" "Hey there you little fuck, you are running out of battery space it's only fucking ${battery}%, go fucking charge it, move that lazy ass." 
+    # Get battery device
+    battery_path=$(upower -e | grep BAT)
+    
+    # Get battery percentage (remove %)
+    battery=$(upower -i "$battery_path" | awk '/percentage:/ {gsub(/%/,""); print $2}')
+    
+    # Get charging state (e.g., "charging", "discharging", "fully-charged")
+    state=$(upower -i "$battery_path" | awk '/state:/ {print $2}')
+    
+    # Only warn if discharging and battery ≤ 20%
+    if [ "$state" = "discharging" ] && [ "$battery" -le 20 ]; then
+        notify-send -u critical "Low Battery" \
+        "Hey there you little fuck, you're running out of battery, it's only ${battery}%, go charge that shit up." 
         sleep 240
     else
         sleep 120
